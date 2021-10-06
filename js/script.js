6cfa011aa2c7f9afe6099467b88fc137d83c0330
Bhargavmainam <69231966+Bhargavmainam@users.noreply.github.com>
2021-10-06 07:52:04 +0530

    added transitions

---
+++ js/script.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function ($) {
+    // toggle nav menu on button click
+    var $root = $('html, body'),
+        menu_toggle = $('.menu-toggle');
+    menu_toggle.on('click', function () {
+        var $this = $(this);
+
+        $this.toggleClass('close');
+        $('.menu, .menu-nav, .menu-nav-item').toggleClass('show');
+    });
+});
